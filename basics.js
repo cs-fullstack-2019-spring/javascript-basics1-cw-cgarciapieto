91d58fcb9f504e5a3f53ce51884fb94f4e495b92
carlos <cgarciapieto@gmail.com>
2019-01-18 12:50:36 -0600

    1stclasswor

---
+++ basics.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+var nameVariable = "carlos";
+console.log("hello" + nameVariable);
+
+var dogAge = "";
+
+console.log("Your dog is: " + dogAge * 7);
+
+var num1 = 1;
+var num2 = 1;
+
+var total = num1 + num2;
+console.log("the total is ", total);
